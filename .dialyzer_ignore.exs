4a67ef25e7743f0dc570ec6c770534478b2e461c
Luca Corti <luca@sibill.com>
2024-10-01 14:25:25 +0200

    Fix dialyxir

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,7 @@
+[
+  {"src/AttributeCertificateVersion1.erl", :unknown_function},
+  {"src/CryptographicMessageSyntax2004.erl", :unknown_function},
+  {"src/PKIX1Explicit88.erl", :unknown_function},
+  {"src/PKIX1Implicit88.erl", :unknown_function},
+  {"src/PKIXAttributeCertificate.erl", :unknown_function},
+]
